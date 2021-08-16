b088d15093dcdc542c2106edeaa885361d6a996d
Jean Marcel <jeanvanzella91@gmail.com>
2021-08-16 04:29:38 -0300

    req 8 e 12

--- src/zoo.js
+++ src/zoo.js
@@ -1,4 +1,4 @@
-const { employees, species } = require('./data');
+const { employees, species, hours, prices } = require('./data');
 const data = require('./data');
 
 function getSpeciesByIds(ids) {
@@ -43,12 +43,27 @@ function countAnimals(specie) {
   return animalName[0].residents.length;
 }
 
-function calculateEntry(entrants) {
-  // seu código aqui
+function calculateEntry(entrants = {}) {
+  let accumulator = 0;
+  const { Adult = 0, Child = 0, Senior = 0 } = entrants;
+  const adultos = Adult * prices.Adult;
+  const seniors = Senior * prices.Senior;
+  const criancas = Child * prices.Child;
+  accumulator = criancas + seniors + adultos;
+  return accumulator;
 }
 
 function getAnimalMap(options) {
-  // seu código aqui
+  if (options === undefined) {
+    const exer10 = {}
+      exerc10.Friday = "Open from 10am until 8pm",
+      exerc10.Monday = "CLOSED", 
+      exerc10.Saturday = "Open from 8am until 10pm", 
+      exerc10.Sunday = "Open from 8am until 8pm", 
+      exerc10.Thursday ="Open from 10am until 8pm", 
+      exerc10.Tuesday = "Open from 8am until 6pm", 
+      exerc10.Wednesday = "Open from 8am until 6pm"
+  }
 }
 
 function getSchedule(dayName) {
@@ -60,7 +75,11 @@ function getOldestFromFirstSpecies(id) {
 }
 
 function increasePrices(percentage) {
-  // seu código aqui
+  prices.Adult = parseFloat(Math.fround(prices.Adult * ((percentage / 100) + 1)).toPrecision(4));
+  prices.Child = parseFloat(Math.fround(prices.Child * ((percentage / 100) + 1)).toPrecision(4));
+  prices.Senior = parseFloat(Math.fround(prices.Senior * ((percentage / 100) + 1)).toPrecision(4));
+  return prices;
+  // estava dando erro antes pois recebia uma string, o parsefloat transoforma em numero, eu acho?
 }
 
 function getEmployeeCoverage(idOrName) {
